ba00aa865245a9852d3a3fdcfe320d3235245427
Brian Turek <brian.turek@gmail.com>
2020-04-22 11:59:38 +0100

    Add websocket reconnect logic

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,15 @@
+"use strict";
+
 const trackedIds = new Set();
 
 /**
  * Adds a completeled download to the downloads table on the UI.
  *
- * @param {string} pretty_name Human-readable name of the download.
+ * @param {string} prettyName Human-readable name of the download.
  * @param {string} key Unique key that identifies this download (use for deletes).
  * @param {string} path Relative path to the download directory for this item.
  */
-function createDownloadListing(pretty_name, key, path) {
+function createDownloadListing(prettyName, key, path) {
   const table = document.getElementById("availableFiles"),
     tbody = table.getElementsByTagName("tbody")[0],
     newRow = tbody.insertRow(0),
@@ -16,7 +18,7 @@ function createDownloadListing(pretty_name, key, path) {
     managementCell = newRow.insertCell();
 
   newRow.id = key;
-  nameCell.innerHTML = pretty_name;
+  nameCell.innerHTML = prettyName;
   downloadCell.innerHTML = `<a href="${path}">Download</a>`;
   managementCell.innerHTML = `<i class="fas fa-trash" style="color: red" onclick="removeDownload('${key}')"></i>`;
   table.classList.remove("is-hidden");
@@ -61,7 +63,11 @@ function download() {
   request.onload = function () {
     if (this.status === 202) {
       const resp = JSON.parse(this.response);
-      trackedIds.add(resp["req_id"]);
+      trackedIds.add(resp.req_id);
+      document.getElementById(
+        "downloads"
+      ).innerHTML += `<div id="${resp.req_id}:div"><h3 class="subtitle is-6">Request ${resp.req_id} for ${params.url}</h3></div>`;
+      document.getElementById("noDownloads").classList.add("is-hidden");
     }
   };
 
@@ -93,36 +99,38 @@ function handleWSMessage(event) {
     }
   } else if (msg.status === "COMPLETED") {
     createDownloadListing(msg.pretty_name, msg.key, msg.path);
-    trackedIds.delete(msg["req_id"]);
 
-    if (trackedIds.size === 0) {
-      document.getElementById("noDownloads").classList.remove("is-hidden");
+    if (trackedIds.has(msg.req_id)) {
+      trackedIds.delete(msg["req_id"]);
+
+      const reqDiv = document.getElementById(`${msg.req_id}:div`);
+      if (reqDiv) {
+        reqDiv.remove();
+      }
+
+      if (trackedIds.size === 0) {
+        document.getElementById("noDownloads").classList.remove("is-hidden");
+      }
     }
   } else if (msg.status === "DOWNLOADING" && trackedIds.has(msg.req_id)) {
-    const progress_bar = document.getElementById(
-      `${msg.req_id}:${msg.filename}:progress`
-    );
-    let req_div = document.getElementById(`${msg.req_id}:div`);
-    document.getElementById("noDownloads").classList.add("is-hidden");
+    const progressBar = document.getElementById(
+        `${msg.req_id}:${msg.filename}:progress`
+      ),
+      reqDiv = document.getElementById(`${msg.req_id}:div`);
 
-    if (!req_div) {
-      document.getElementById(
-        "downloads"
-      ).innerHTML += `<div id="${msg.req_id}:div"></div>`;
-      req_div = document.getElementById(`${msg.req_id}:div`);
-    }
-
-    if (progress_bar) {
-      progress_bar.value = (msg.downloaded_bytes / msg.total_bytes) * 100;
+    if (progressBar) {
+      progressBar.value = (msg.downloaded_bytes / msg.total_bytes) * 100;
     } else {
-      req_div.innerHTML += `
+      reqDiv.innerHTML += `
+      <div id="${msg.req_id}:${msg.filename}:div">
         <label class="label">${msg.filename}</label>
-        <progress class="progress" value="0" max="100" id="${msg.req_id}:${msg.filename}:progress">0</progress>`;
+        <progress class="progress" value="0" max="100" id="${msg.req_id}:${msg.filename}:progress">0</progress>
+      </div>`;
     }
   } else if (msg.status === "DOWNLOADED" && trackedIds.has(msg.req_id)) {
-    const req_div = document.getElementById(`${msg.req_id}:div`);
-    if (req_div) {
-      req_div.remove();
+    const dlDiv = document.getElementById(`${msg.req_id}:${msg.filename}:div`);
+    if (dlDiv) {
+      dlDiv.remove();
     }
   }
 }
@@ -151,6 +159,31 @@ function connectWS() {
   const ws_proto = window.location.protocol === "https:" ? "wss" : "ws",
     ws = new WebSocket(`${ws_proto}://${window.location.host}/api/status`);
   ws.onmessage = handleWSMessage;
+  ws.onclose = function () {
+    // Clear the downloaded area
+    const table = document.getElementById("availableFiles");
+    document.getElementById("noAvailableFiles").classList.remove("is-hidden");
+    for (let i = table.rows.length - 1; i > 0; i--) {
+      table.deleteRow(i);
+    }
+    table.classList.add("is-hidden");
+
+    // Clear the downloading area
+    trackedIds.forEach((req_id) => {
+      const reqDiv = document.getElementById(`${req_id}:div`);
+
+      if (reqDiv) {
+        reqDiv.remove();
+      }
+    });
+    document.getElementById("noDownloads").classList.remove("is-hidden");
+
+    // Clear all the tracked IDs
+    trackedIds.clear();
+
+    // Schedule a reconnect
+    setTimeout(connectWS, 3000);
+  };
 }
 
 window.addEventListener("load", connectWS);
